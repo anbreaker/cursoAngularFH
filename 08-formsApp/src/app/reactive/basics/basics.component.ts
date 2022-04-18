b1ae1b345f5021829f425f3ed9cafa3a2e737d07
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-18 13:13:28 +0200

    init html for reactive forms

--- 08-formsApp/src/app/reactive/basics/basics.component.ts
+++ 08-formsApp/src/app/reactive/basics/basics.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent {
+  // myForm: FormGroup = new FormGroup({
+  //   product: new FormControl('RTX 4080Ti'),
+  //   price: new FormControl(1500),
+  //   stocks: new FormControl(5),
+  // });
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    //['RTX 4080Ti',validations, asyncValidations]],
+    product: ['RTX 4080Ti', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stocks: [0, [Validators.required, Validators.min(0)]],
+  });
 
-  ngOnInit(): void {
-  }
-
+  constructor(private fb: FormBuilder) {}
 }
