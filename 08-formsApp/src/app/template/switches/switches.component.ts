5cb7ce7715ccbcce0292e803c094cae3228d093b
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-18 12:31:00 +0200

    dinamics templates form angular end

--- 08-formsApp/src/app/template/switches/switches.component.ts
+++ 08-formsApp/src/app/template/switches/switches.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
+  person = {
+    gender: 'F',
+    notifications: false,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  termsAndConditions: boolean = false;
 }
