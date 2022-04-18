c02f16be3ed2c6757301ff6a7a0237ae487fd952
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-18 11:21:29 +0200

    dinamics templates examples

--- 08-formsApp/src/app/template/basics/basics.component.ts
+++ 08-formsApp/src/app/template/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
--- 08-formsApp/src/app/template/dinamics/dinamics.component.ts
+++ 08-formsApp/src/app/template/dinamics/dinamics.component.ts
@@ -1,16 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
+  @ViewChild('myForm') myForm!: NgForm;
 
-  constructor() { }
+  newGame: string = '';
 
-  ngOnInit(): void {
+  person: Person = {
+    name: 'anbreaker',
+    favorites: [
+      { id: 1, name: 'Spyro' },
+      { id: 2, name: 'Far Cry' },
+    ],
+  };
+
+  validName(): boolean {
+    console.log(this.myForm?.form);
+
+    return (
+      this.myForm?.form.controls['name']?.invalid &&
+      this.myForm?.form.controls['name']?.touched
+    );
   }
 
+  delete(index: number) {
+    this.person.favorites.splice(index, 1);
+  }
+
+  addGame() {
+    const newGame: Favorite = {
+      id: this.person.favorites.length + 1,
+      name: this.newGame,
+    };
+
+    this.person.favorites.push({ ...newGame });
+    this.newGame = '';
+  }
+
+  save() {
+    this.addGame();
+    console.log('click save');
+  }
 }
