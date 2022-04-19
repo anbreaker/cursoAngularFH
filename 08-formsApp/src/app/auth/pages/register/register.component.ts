2dccb1966cd849330b9031e7fdd4ae7cb99d2e08
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-19 09:06:57 +0200

    custom validations without services

--- 08-formsApp/src/app/auth/pages/register/register.component.ts
+++ 08-formsApp/src/app/auth/pages/register/register.component.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import {
+  nameSurname,
+  emailPattern,
+  usernameExists,
+} from '../../../shared/validators/validations';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styles: [],
 })
 export class RegisterComponent implements OnInit {
-  // TODO temp
-  nameSurname: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
-
   myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(this.nameSurname)]],
-    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    name: ['', [Validators.required, Validators.pattern(nameSurname)]],
+    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+    username: ['', [Validators.required, usernameExists]],
   });
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.myForm.reset({
-      name: 'Magalí Antúnez',
-      email: 'magali@rootdevs.com',
+      name: 'Magali Antunez',
+      email: 'anbreaker@rootdevs.com',
+      username: 'anbreaker',
     });
   }
 
