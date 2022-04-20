35bf768b1813bc115b526a5274c72ce0357d2ed1
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-20 16:59:22 +0200

    add input countries, and use rxjs switchmap

--- 09-selectors/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 09-selectors/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { CountrySmall } from '../../interfaces/country.interface';
 import { CountryServices } from '../../services/country.service';
@@ -12,9 +13,9 @@ import { CountryServices } from '../../services/country.service';
 export class SelectorPageComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
   });
 
-  // Selectors
   regions: string[] = [];
   countries: CountrySmall[] = [];
 
@@ -26,17 +27,30 @@ export class SelectorPageComponent implements OnInit {
   ngOnInit(): void {
     this.regions = this.countryServices.regions;
 
-    this.myForm.get('region')?.valueChanges.subscribe((region) => {
-      console.log(region);
+    // this.myForm.get('region')?.valueChanges.subscribe((region) => {
+    //   console.log(region);
 
-      this.countryServices
-        .getCountriesByRegion(region)
-        .subscribe((countries) => {
-          console.log(countries);
+    //   this.countryServices
+    //     .getCountriesByRegion(region)
+    //     .subscribe((countries) => {
+    //       this.countries = countries;
 
-          this.countries = countries;
-        });
-    });
+    //       console.log(this.countries);
+    //     });
+    // });
+
+    this.myForm
+      .get('region')
+      ?.valueChanges.pipe(
+        tap((_) => this.myForm.get('country')?.reset('')),
+
+        switchMap((region) => this.countryServices.getCountriesByRegion(region))
+      )
+      .subscribe((contries) => {
+        this.countries = contries;
+
+        console.log(this.countries);
+      });
   }
 
   save() {
