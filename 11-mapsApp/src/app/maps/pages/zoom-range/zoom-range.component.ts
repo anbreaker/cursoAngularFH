61adde5d97e2e9583f4f16b6c0bd6b60bcdcccc2
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-27 18:30:16 +0200

    init page fullscreen and zoom

--- 11-mapsApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ 11-mapsApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .map-container {
+        height: 100%;
+        width: 100%;
+      }
+
+      .row {
+        background-color: #002b36;
+        border-radius: 5px;
+        color: aliceblue;
+        padding: 10px;
+        position: fixed;
+        top: 80%;
+        z-index: 10;
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let mapZoom = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-6.97708841262686, 38.87720482451688],
+      zoom: 14,
+    });
   }
-
 }
