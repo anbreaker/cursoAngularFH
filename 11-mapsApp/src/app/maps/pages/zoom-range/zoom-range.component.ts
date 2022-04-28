3df61e8237183aa7648e1a08d44d538f3b6555c3
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-04-28 11:13:27 +0200

    workin with zoom

--- 11-mapsApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ 11-mapsApp/src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
@@ -17,20 +17,47 @@ import * as mapboxgl from 'mapbox-gl';
         padding: 10px;
         position: fixed;
         top: 80%;
+        width: 300px;
         z-index: 10;
       }
     `,
   ],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('map') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel: number = 10;
+
   constructor() {}
 
-  ngOnInit(): void {
-    let mapZoom = new mapboxgl.Map({
-      container: 'map',
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-6.97708841262686, 38.87720482451688],
-      zoom: 14,
+      zoom: this.zoomLevel,
     });
+
+    this.map.on('zoom', (ev) => {
+      this.zoomLevel = this.map.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map.getZoom() > 18) {
+        this.map.zoomTo(18);
+      }
+    });
+  }
+
+  zoomIn() {
+    this.map.zoomIn();
+  }
+
+  zoomOut() {
+    this.map.zoomOut();
+  }
+
+  changeZoom(zoom: string) {
+    this.map.zoomTo(+zoom);
   }
 }
