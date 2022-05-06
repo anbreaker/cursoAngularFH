ede7cb009668311fc66c4de08a0bf54ed5b48bf0
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-06 14:56:01 +0200

    ngtochart configurated

--- 12-chartsApp/src/app/shared/menu/menu.component.ts
+++ 12-chartsApp/src/app/shared/menu/menu.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface MenuItem {
+  route: string;
+  text: string;
+}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  menu: MenuItem[] = [
+    { route: '/charts/bars', text: 'Bars' },
+    { route: '/charts/double-bars', text: 'Double-bars' },
+    { route: '/charts/dona', text: 'Dona' },
+    { route: '/charts/dona-http', text: 'Dona-http' },
+  ];
 }
