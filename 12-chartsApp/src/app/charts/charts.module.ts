0b61c422e75f154b36328ed7caa555f5ae89e9c1
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-06 14:00:48 +0200

    init charts, only routing ando module configuration

---
+++ 12-chartsApp/src/app/charts/charts.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChartsRoutingModule } from './charts-routing.module';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { ChartBarComponent } from './components/chart-bar/chart-bar.component';
+import { DonaComponent } from './pages/dona/dona.component';
+
+
+@NgModule({
+  declarations: [
+    BarsComponent,
+    DoubleBarsComponent,
+    DonaHttpComponent,
+    ChartBarComponent,
+    DonaComponent
+  ],
+  imports: [
+    CommonModule,
+    ChartsRoutingModule
+  ]
+})
+export class ChartsModule { }
