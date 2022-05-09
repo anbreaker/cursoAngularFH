e89bdc0c5c1d2fefb64a1d192eb81452a3c6b9e2
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-09 16:10:28 +0200

    chart dona http with service

--- 12-chartsApp/src/app/charts/services/charts.service.ts
+++ 12-chartsApp/src/app/charts/services/charts.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, delay } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChartsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getSocialNetworksUsers() {
+    return this.http.get('http://localhost:3000/chart');
+  }
+
+  getSocialNetworksUsersForDona() {
+    return this.getSocialNetworksUsers().pipe(
+      delay(1200),
+      map((data) => {
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+        return { labels, values };
+      })
+    );
+  }
 }
