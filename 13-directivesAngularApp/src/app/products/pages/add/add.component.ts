cc1b65b0aecd4480feb0ecc4a88831b3b33bef29
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-09 17:12:20 +0200

    reactive input simple configuration

--- 13-directivesAngularApp/src/app/products/pages/add/add.component.ts
+++ 13-directivesAngularApp/src/app/products/pages/add/add.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
-export class AddComponent implements OnInit {
+export class AddComponent {
+  myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  hasError(field: string): boolean {
+    return this.myForm.get(field)?.invalid || false;
   }
-
 }
