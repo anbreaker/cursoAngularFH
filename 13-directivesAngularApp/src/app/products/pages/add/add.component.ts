80db5e5b8dac9ec12e9e3a619d57b0289cc93b2f
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-10 10:45:22 +0200

    custom span directive

--- 13-directivesAngularApp/src/app/products/pages/add/add.component.ts
+++ 13-directivesAngularApp/src/app/products/pages/add/add.component.ts
@@ -7,6 +7,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
+  message1: string = 'anbreaker';
+  color: string = 'green';
+
   myForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
   });
@@ -15,5 +18,19 @@ export class AddComponent {
 
   hasError(field: string): boolean {
     return this.myForm.get(field)?.invalid || false;
+  }
+
+  changeName(): void {
+    this.message1 = Math.random().toString();
+  }
+
+  changeColor(): void {
+    const color = `#${crypto
+      .getRandomValues(new Uint32Array(1))[0]
+      .toString(16)
+      .padStart(8, '0')
+      .slice(-6)}`;
+
+    this.color = color;
   }
 }
