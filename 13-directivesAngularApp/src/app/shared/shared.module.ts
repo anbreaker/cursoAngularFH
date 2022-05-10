8c9cda05f1eb2deae4ee1c4d25b32bbc4c314b1d
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-05-10 11:01:07 +0200

    structural directive

--- 13-directivesAngularApp/src/app/shared/shared.module.ts
+++ 13-directivesAngularApp/src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 @NgModule({
-  declarations: [ErrorMsgDirective],
-  exports: [ErrorMsgDirective],
+  declarations: [ErrorMsgDirective, CustomIfDirective],
+  exports: [ErrorMsgDirective, CustomIfDirective],
 })
 export class SharedModule {}
